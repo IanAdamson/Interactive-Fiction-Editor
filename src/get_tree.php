48740c41fb39acc44ef96194bd8cf83f9eec7e57
IanAdamson <fbodragoon@gmail.com>
2014-03-15 20:59:43 -0700

    Pruning code

--- src/get_tree.php
+++ src/get_tree.php
@@ -3,7 +3,7 @@
  * get_tree.php
  * 
  * Retrieves the whole dialog tree from the database and outputs it in
- * a file tree-style format.
+ * a file tree-style format. Yay recursion
  ******************************************************************************/
 
 require('inc/database.php');
@@ -16,12 +16,12 @@ function getChildren($nodeID) {
     $result = mysql_query($sql);
     
     if(mysql_num_rows($result) != 0) {
-        echo '<ul class="jqueryFileTree">'."\n";
+        echo '<ul>'."\n";
         while( $result2 = mysql_fetch_array($result) ) {
             if($nodeID == '0') {
-                echo '<li class="directory collapsed"><a href="#" rel="' . $result2['n_id'] . '" class="dialogNode">' . $result2['n_name'] . '</a></li>'."\n";
+                echo '<li><a href="#" rel="' . $result2['n_id'] . '" class="dialogNode">' . $result2['n_name'] . '</a></li>'."\n";
             } else {
-                echo '<li class="directory collapsed"><a href="#" rel="' . $result2['n_id'] . '" class="dialogNode">' . $result2['n_action'] . '</a></li>'."\n";
+                echo '<li><a href="#" rel="' . $result2['n_id'] . '" class="dialogNode">' . $result2['n_action'] . '</a></li>'."\n";
             }
             getChildren($result2['n_id']);
         }
@@ -31,31 +31,3 @@ function getChildren($nodeID) {
 }
 
 getChildren('0');
-
-/* // Old tree view
-$nodeID = urldecode($_POST['dir']);
-$nodeID = str_replace("/", "", $nodeID);
-
-if ( is_numeric($nodeID) ) {
-    echo '<ul class="jqueryFileTree" style="display: none;">';
-    
-    // Get current node
-    $sql = "SELECT * FROM `nodes` WHERE `n_parent` = '".$nodeID."'";
-    
-    $results = mysql_query($sql);
-    
-    while( $result = mysql_fetch_array($results) ) {
-        if($nodeID == '0') {
-            echo '<li class="directory collapsed"><a href="#" rel="' . $result['n_id'] . '" class="dialogNode">' . $result['n_name'] . '</a></li>';
-        } else {
-            echo '<li class="directory collapsed"><a href="#" rel="' . $result['n_id'] . '" class="dialogNode">' . $result['n_action'] . '</a></li>';
-        }
-        
-    }
-
-    //  getChildNodes($id)
-    echo '</ul>';
-}
-*/
-
-/* End of file get_tree.php */
